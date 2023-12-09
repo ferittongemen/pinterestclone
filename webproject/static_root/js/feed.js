cf15dec807c84f42fb4ef99a7b14ef2b185c74d4
ferittongemen <148661211+ferittongemen@users.noreply.github.com>
2023-12-09 17:38:19 +0300

    Delete webproject/static_root directory

--- webproject/static_root/js/feed.js
+++
@@ -1,9 +0,0 @@
-function showDetails(imageUrl, description) {
-    document.getElementById("modalImage").src = imageUrl;
-    document.getElementById("modalDescription").innerHTML = description;
-    document.getElementById("myModal").style.display = "flex";
-}
-
-function closeModal() {
-    document.getElementById("myModal").style.display = "none";
-}
